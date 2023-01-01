45eaf65a5fea665beca13fcf25c680a6ef4996b1
FatiZaha <121752209+FatiZaha@users.noreply.github.com>
2023-01-01 12:40:05 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,142 @@
+var prog = [{
+    image: './images/javascriptDef.png',
+    title: 'what is javascript?',
+    category: 'JS',
+    pos:1,
+    price: 9.9
+
+},
+{
+    image: './images/htmlBasics.PNG',
+    title: 'basics of HTML',
+    category: 'HTML',
+    pos:2,
+    price: 5.9
+},
+{
+    image: './images/htmlElements.png',
+    title: 'HTML elements and tags',
+    category: 'HTML',
+    pos:3,
+    price: 9.9
+},
+{
+    image: './images/cssSelectors.jpg',
+    title: 'CSS selectors',
+    category: 'CSS',
+    pos:4,
+    price: 69.9
+},
+{
+    image: './images/javascriptVariables.png',
+    title: 'variables and data type of javascript',
+    category: 'JS',
+    pos:5,
+    price: 19.9
+},
+{
+    image: './images/javascriptOperators.png',
+    title: 'Javascript operators and conditions',
+    category: 'JS',
+    pos:6,
+    price: 29.9
+},
+{
+    image: './images/htmlAttrVal.jpg',
+    title: 'HTML attributes and values',
+    category: 'HTML',
+    pos:7,
+    price: 19.9
+},
+{
+    image: './images/cssProperties.png',
+    title: 'CSS properties',
+    category: 'CSS',
+    pos:8,
+    price: 29.9
+},
+{
+    image: './images/javascriptObjects.png',
+    title: 'Javascript objects and arrays',
+    category: 'JS',
+    pos:9,
+    price: 39.9
+},
+{
+    image: './images/cssMesures.png',
+    title: 'mesures and unites of CSS',
+    category: 'CSS',
+    pos:10,
+    price: 19.9
+},
+{
+    image: './images/cssAnimation.png',
+    title: 'CSS animations',
+    category: 'CSS',
+    pos:11,
+    price: 19.9
+},
+{
+    image: './images/javascriptFunctions.png',
+    title: 'The basics of javascript',
+    category: 'JS',
+    pos:12,
+    price: 29.9
+},
+{
+    image: './images/javascriptEvents.png',
+    title: 'javascript events',
+    category: 'JS',
+    pos:13,
+    price: 59.9
+},
+{
+    image: './images/cssColors.png',
+    title: 'css colors',
+    category: 'css',
+    pos:14,
+    price: 29.9
+},
+{
+    image: './images/phpBasics.png',
+    title: 'getting started with php',
+    category: 'php',
+    pos:15,
+    price: 15.9
+},
+{
+    image: './images/javascriptFunctions2.png',
+    title: 'functions and loops with javascript',
+    category: 'JS',
+    pos:16,
+    price: 19.9
+},
+{
+    image: './images/phpDataBase.png',
+    title: 'connecting to database using PHP',
+    category: 'PHP',
+    pos:17,
+    price: 29.9
+},
+{
+    image: './images/phpCRUD.png',
+    title: 'manipulating crud using php',
+    category: 'php',
+    pos:18,
+    price: 45.9
+},
+{
+    image: './images/javascriptDOM.png',
+    title: 'DOM the power of javascript',
+    category: 'JS',
+    pos:19,
+    price: 23.9
+},
+{
+    image: './images/javascriptDOM.png',
+    title: 'DOM the power of javascript',
+    category: 'JS_g4',
+    pos:20,
+    price: 23.9
+}
+]
